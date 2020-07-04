baa9e9400b40ce361322daa3ebeff3dc545dde7e
Ionut Octavian ILIE <iilie@bitdefender.com>
2020-07-05 02:21:04 +0300

    Added Mp3Cutter

---
+++ Mp3Cutter/Form1.Designer.cs
@@ -0,0 +1,346 @@
+﻿using NAudio.Wave;
+
+namespace Mp3Cutter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.loadButton = new System.Windows.Forms.Button();
+            this.textBoxMp3 = new System.Windows.Forms.TextBox();
+            this.waveViewer1 = new NAudio.Gui.WaveViewer();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // loadButton
+            // 
+            this.loadButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loadButton.Location = new System.Drawing.Point(697, 20);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(75, 29);
+            this.loadButton.TabIndex = 0;
+            this.loadButton.Text = "Load MP3";
+            this.loadButton.UseVisualStyleBackColor = true;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // textBoxMp3
+            // 
+            this.textBoxMp3.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxMp3.Location = new System.Drawing.Point(12, 20);
+            this.textBoxMp3.Name = "textBoxMp3";
+            this.textBoxMp3.ReadOnly = true;
+            this.textBoxMp3.Size = new System.Drawing.Size(679, 29);
+            this.textBoxMp3.TabIndex = 1;
+            // 
+            // waveViewer1
+            // 
+            this.waveViewer1.Location = new System.Drawing.Point(25, 178);
+            this.waveViewer1.Name = "waveViewer1";
+            this.waveViewer1.SamplesPerPixel = 128;
+            this.waveViewer1.Size = new System.Drawing.Size(737, 149);
+            this.waveViewer1.StartPosition = ((long)(0));
+            this.waveViewer1.TabIndex = 2;
+            this.waveViewer1.WaveStream = null;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.LargeChange = 4;
+            this.trackBar1.Location = new System.Drawing.Point(12, 90);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(760, 45);
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.LargeChange = 1;
+            this.trackBar2.Location = new System.Drawing.Point(12, 167);
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(760, 45);
+            this.trackBar2.TabIndex = 4;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            this.trackBar2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.trackBar2_MouseDown);
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.LargeChange = 1;
+            this.trackBar3.Location = new System.Drawing.Point(12, 304);
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(760, 45);
+            this.trackBar3.TabIndex = 5;
+            this.trackBar3.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 29);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Play";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(93, 55);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 29);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Pause";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(697, 55);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 29);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Export MP3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(174, 55);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(97, 29);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Play Selection";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(46, 141);
+            this.textBox1.MaxLength = 64;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 10;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(203, 141);
+            this.textBox2.MaxLength = 64;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 144);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Start";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(171, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "End";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(460, 62);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(99, 17);
+            this.checkBox1.TabIndex = 14;
+            this.checkBox1.Text = "Keep Metadata";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(565, 55);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(126, 29);
+            this.progressBar1.TabIndex = 15;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(368, 141);
+            this.textBox3.MaxLength = 64;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 16;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(321, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Current";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(283, 63);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 13);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Granularity (ms)";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Location = new System.Drawing.Point(368, 61);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(53, 20);
+            this.numericUpDown1.TabIndex = 20;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            250,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 361);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.waveViewer1);
+            this.Controls.Add(this.textBoxMp3);
+            this.Controls.Add(this.loadButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MP3 Cutter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.TextBox textBoxMp3;
+        private NAudio.Gui.WaveViewer waveViewer1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+
+
+}
+
---
+++ Mp3Cutter/Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.Wave;
+using System.Windows.Forms;
+using NAudio.Gui;
+using NAudio.Wave.SampleProviders;
+using System.IO;
+
+namespace Mp3Cutter
+{
+    public partial class Form1 : Form
+    {
+        private WaveStream pcm = null;
+        private Mp3FileReader mp3FileReader = null;
+        private WaveOut output = null;
+        private BlockAlignReductionStream stream = null;
+        private int granularity = 250;
+
+        Timer timer = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void loadButton_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value = 0;
+
+            if (openFileDialog1.ShowDialog() != DialogResult.OK)
+                return;
+
+            output?.Stop();
+            output?.Dispose();
+            timer?.Stop();
+
+            var fname = openFileDialog1.FileName;
+
+            if (fname.EndsWith(".mp3"))
+            {
+                mp3FileReader = new Mp3FileReader(fname);
+                pcm = WaveFormatConversionStream.CreatePcmStream(mp3FileReader);
+            }
+
+            else
+            {
+                MessageBox.Show("File is not mp3!");
+                return;
+            }
+
+            granularity = (int) numericUpDown1.Value;
+            textBoxMp3.Text = fname;
+
+            stream = new BlockAlignReductionStream(pcm);
+            output = new WaveOut();
+            output.DesiredLatency = 500;
+            output.Init(stream);
+
+            waveViewer1.WaveStream = stream;
+
+
+            trackBar1.Maximum = (int)(stream.TotalTime.TotalSeconds * (1000.0 / granularity));
+            trackBar1.Value = 0;
+
+            trackBar2.Maximum = (int)(stream.TotalTime.TotalSeconds * (1000.0 / granularity));
+            trackBar2.Value = 0;
+
+            trackBar3.Maximum = (int)(stream.TotalTime.TotalSeconds * (1000.0 / granularity));
+            trackBar3.Value = trackBar3.Maximum;
+
+            timer = new Timer();
+            timer.Interval = (int)(granularity / 2 - 10);
+            timer.Tick += new EventHandler(timer_Tick);
+            
+            textBox1.Text = TimeSpan.FromMilliseconds(0).ToShortForm();
+            textBox2.Text = TimeSpan.FromMilliseconds(stream.TotalTime.TotalMilliseconds).ToShortForm();
+        }
+
+        public void timer_Tick(object sender, EventArgs e)
+        {
+            double ms = stream.GetMs(output);
+            trackBar1.Value = (int)(ms / granularity);
+
+            if (trackBar1.Value >= trackBar3.Value)
+                output.Stop();
+
+            if (output.PlaybackState == PlaybackState.Stopped)
+                timer.Stop();
+
+            textBox3.Text = TimeSpan.FromMilliseconds(ms).ToShortForm();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "mp3 files (*.mp3)|*.mp3";
+            openFileDialog1.RestoreDirectory = true;
+
+            trackBar3.Value = trackBar3.Maximum;
+
+            numericUpDown1.Value = 250;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (output == null)
+                return;
+
+            double ms = trackBar1.GetMs(granularity);
+            stream.Position = (long)(ms * output.OutputWaveFormat.SampleRate * output.OutputWaveFormat.BitsPerSample * output.OutputWaveFormat.Channels / 8000.0) & ~1;
+
+            output?.Play();
+            timer?.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer?.Stop();
+            output?.Pause();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            if (stream == null)
+                return;
+
+            double ms = trackBar1.GetMs(granularity);
+            stream.Position = (long)(ms * output.OutputWaveFormat.SampleRate * output.OutputWaveFormat.BitsPerSample * output.OutputWaveFormat.Channels / 8000.0) & ~1;
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            if (trackBar2.Value >= trackBar3.Value)
+                trackBar2.Value = trackBar3.Value - 1;
+
+            trackBar1.Value = trackBar2.Value;
+
+            trackBar1_Scroll(null, null);
+            textBox1.Text = TimeSpan.FromMilliseconds(trackBar2.GetMs(granularity)).ToShortForm();
+        }
+
+        private void trackBar2_MouseDown(object sender, MouseEventArgs e)
+        {
+            trackBar2_Scroll(null, null);
+            if (output?.PlaybackState == PlaybackState.Stopped)
+            {
+                output.Play();
+                timer.Start();
+            }
+        }
+
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            if (trackBar3.Value <= trackBar2.Value)
+                trackBar3.Value = trackBar2.Value + 1;
+
+            textBox2.Text = TimeSpan.FromMilliseconds(trackBar3.GetMs(granularity)).ToShortForm();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.FileName = openFileDialog1.FileName.Replace(".mp3", "_cut.mp3");
+
+            if (saveFileDialog1.ShowDialog() != DialogResult.OK)
+                return;
+
+            var start = trackBar2.GetMs(granularity);
+            var end = trackBar3.GetMs(granularity);
+
+            try
+            {
+                using (var reader = new Mp3FileReader(openFileDialog1.FileName))
+                using (var writer = File.Create(saveFileDialog1.FileName))
+                {
+                    progressBar1.Maximum = (int)reader.TotalTime.TotalSeconds;
+
+                    if (reader.Id3v2Tag != null && checkBox1.Checked)
+                        writer.Write(reader.Id3v2Tag.RawData, 0, reader.Id3v2Tag.RawData.Length);
+
+                    Mp3Frame frame;
+                    while ((frame = reader.ReadNextFrame()) != null)
+                        if (reader.CurrentTime >= TimeSpan.FromMilliseconds(start))
+                        {
+                            progressBar1.Value = (int)reader.CurrentTime.TotalSeconds;
+
+                            if (reader.CurrentTime <= TimeSpan.FromMilliseconds(end))
+                                writer.Write(frame.RawData, 0, frame.RawData.Length);
+                            else break;
+                        }
+
+                    progressBar1.Value = progressBar1.Maximum;
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            trackBar2_Scroll(null, null);
+            output?.Play();
+            if (output != null)
+                timer.Start();
+        }
+
+    }
+
+    static class Extensions
+    {
+        public static string ToShortForm(this TimeSpan t)
+        {
+            string shortForm = "";
+            if (t.Hours > 0)
+            {
+                shortForm += string.Format("{0}:", t.Hours.ToString());
+            }
+            
+            shortForm += string.Format("{0}:{1}:{2}", t.Minutes, t.Seconds, t.Milliseconds);
+            
+            return shortForm;
+        }
+        public static double GetMs(this NAudio.Wave.BlockAlignReductionStream stream, WaveOut output)
+        {
+            return stream.Position * 1000.0 / output.OutputWaveFormat.BitsPerSample / output.OutputWaveFormat.Channels * 8.0 / output.OutputWaveFormat.SampleRate;
+        }
+        public static double GetMs(this System.Windows.Forms.TrackBar tb, int granularity)
+        {
+            return tb.Value * granularity;
+        }
+    }
+}
